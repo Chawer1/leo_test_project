65109f4b7b30988876a2a31dfd3a68ada2b3c5a8
Kiryl Charnyshou <Chawert@protonmail.com>
2024-02-03 18:06:18 +0100

    Compare commit

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  def after_sign_in_path_for(resource)
-    if resource.sign_in_count == 1
-      teams_path
-    else
-      super
-    end
-  end
-end
